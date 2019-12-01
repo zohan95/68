7a042fc67383ebf662f0320501659ee32f7ca48a
Ваше Имя <you@example.com>
2019-12-01 11:56:14 +0600

    HW68 done stage 1

--- source/main/urls.py
+++ source/main/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from webapp.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('add/', api_add),
+    path('substract/', api_sub),
+    path('multiply', api_mult),
+    path('divide/', api_div)
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+import json
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+
+@csrf_exempt
+def api_add(request, *args, **kwargs):
+    request_data = None
+    if request.body:
+        request_data = json.loads(request.body)
+    a = request_data['A']
+    b = request_data['B']
+    data = {
+        'answer': a + b
+    }
+    return JsonResponse(data)
+
+
+@csrf_exempt
+def api_sub(request):
+    request_data = None
+    if request.body:
+        request_data = json.loads(request.body)
+    a = request_data['A']
+    b = request_data['B']
+    data = {
+        'answer': a - b
+    }
+    return JsonResponse(data)
+
+
+@csrf_exempt
+def api_mult(request):
+    request_data = None
+    if request.body:
+        request_data = json.loads(request.body)
+    a = request_data['A']
+    b = request_data['B']
+    data = {
+        'answer': a * b
+    }
+    return JsonResponse(data)
+
+
+@csrf_exempt
+def api_div(request):
+    request_data = None
+    if request.body:
+        request_data = json.loads(request.body)
+    a = request_data['A']
+    b = request_data['B']
+    data = {
+        'answer': a / b
+    }
+    return JsonResponse(data)
